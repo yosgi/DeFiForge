e4706a6e93a4bb42dec06e84a96ba4ce89042efa
Yosgi <siqi.liu@nextspace.com>
2025-02-21 17:04:02 +1300

    turbo manage momorepo

--- gamefi/global.d.ts
+++
@@ -1,8 +0,0 @@
-interface Ethereum {
-    request: (args: any) => Promise<any>;
-    on: (event: string, handler: (...args: any[]) => void) => void;
-  }
-  
-  interface Window {
-    ethereum?: Ethereum;
-  }
